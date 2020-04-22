7bd8454dbdc59d21a11ad06e9235054df930a6cf
coreykoz <ckozlovski2017@gmail.com>
2020-04-22 13:01:50 -0400

    moved html into foldeR

---
+++ server-main.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+import { Database } from './mongo-database';
+import { MyServer } from './myserver-post';
+
+const theDatabase = new Database('coreykoz'); // CHANGE THIS
+const theServer = new MyServer(theDatabase);
+
+theServer.listen(8080);
