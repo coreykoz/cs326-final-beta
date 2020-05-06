84db84cc2b8a0a8b238bc8a2b80396db95fc3613
dhruvivora <dvora@umass.edu>
2020-05-06 13:01:13 -0400

    changed server

--- html/client.ts
+++ html/client.ts
@@ -1,5 +1,5 @@
-//const url = "https://cryptic-eyrie-49046.herokuapp.com/uwallet";
-const url = "http://localhost:8080/uwallet";
+const url = "https://cryptic-eyrie-49046.herokuapp.com/uwallet";
+//const url = "http://localhost:8080/uwallet";
 
 //canvas for the graphs
 const ctx = (<HTMLCanvasElement>document.getElementById('monthySpending'));
@@ -169,33 +169,6 @@ async function updateBudget(){
 
 
 // DELETES
-
-/*
-function deleteMonthly(){
-    (async () => {
-        let expenseName = (<HTMLInputElement>document.getElementById("monthlyName")).value;
-
-        let data = { 'expense_name': expenseName, 'id': "monthly"};
-        const newURL = url + "/deleteMonthly"; 
-        const resp = await postData(newURL, data);
-        const j = await resp.json();
-
-	})();
-}
-
-function deleteBudget(){
-    (async () => {
-        let budgetCate = (<HTMLInputElement>document.getElementById("budgetCategory")).value;
-
-        let data = { 'budget_category': budgetCate, 'id': "budget"};
-        const newURL = url + "/deleteBudget"; 
-        const resp = await postData(newURL, data);
-        const j = await resp.json();
-
-	})();
-}
-*/
-
 function deleteMonthly(){
     (async () => {
         let expenseName = (<HTMLInputElement>document.getElementById("monthlyName")).value;
--- mongo-database.ts
+++ mongo-database.ts
@@ -77,28 +77,6 @@ export class Database {
 		    return null;
 		}
     }
-	
-	/*
-    public async del(name: string, id: string) : Promise<void> {
-		let db = this.client.db(this.dbName);
-		let collection = db.collection(this.collectionName);
-
-		switch(id){
-			case "monthly":
-				var result = await collection.deleteOne({'monthly_expense' : name});
-				break;
-			case "transaction":
-				var result = await collection.deleteOne({'trans_name' : name});
-				break;
-			case "budget":
-				var result = await collection.deleteOne({'budget_category' : name});
-				break;
-				
-		}
-	
-		
-    }
-   */
 
   public async del(name: string, id: string) : Promise<void> {
 	let db = this.client.db(this.dbName);
